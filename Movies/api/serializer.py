dc994477f48933f8d2267b999aec22063a0063fb
nidah <fahmidanida.kk@gmail.com>
2023-04-18 16:03:19 +0530

    view revie..w

--- Movies/api/serializer.py
+++ Movies/api/serializer.py
@@ -39,7 +39,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     user=UserRevSer(read_only=True)
     class Meta:
         model=Review
-        fields=["review","rating"]
+        fields=["review","rating","movie","user"]
     
     def create(self, validated_data):
         user=self.context.get("user")
